bab3d6d0a16769ecaff0999eb56ca45ac297abcd
Gavin D'souza <gavin18d@gmail.com>
2022-12-02 19:20:02 +0530

    fix: ChatRoom.get_members
    Get more info, excluding guest

--- chat/frappe_chat/doctype/chat_room/chat_room.py
+++ chat/frappe_chat/doctype/chat_room/chat_room.py
@@ -1,11 +1,23 @@
 # Copyright (c) 2021, codescientist703 and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ChatRoom(Document):
 	def get_members(self):
+		member_list = []
+
 		if self.members:
-			return [x.strip() for x in self.members.split(",")]
-		return []
+			member_list.extend(x.strip() for x in self.members.split(","))
+
+		if self.guest:
+			member_list.append(frappe.db.get_value("Chat Profile", self.guest, "email"))
+
+		if self.users:
+			member_list.extend(x.user for x in self.users)
+
+		if "Guest" in member_list:
+			member_list.remove("Guest")
+
+		return member_list
